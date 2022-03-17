2305b89f3fad59951109923323bb2b791a0954aa
kristendrex <40031435+kristendrex@users.noreply.github.com>
2022-03-17 12:41:45 -0400

    Update models.scala

--- hw3/src/main/scala/edu/gatech/cse6250/model/models.scala
+++ hw3/src/main/scala/edu/gatech/cse6250/model/models.scala
@@ -2,10 +2,6 @@ package edu.gatech.cse6250.model
 
 import java.sql.Date
 
-/**
- * @author Hang Su <hangsu@gatech.edu>,
- * @author Yu Jing <yjing43@gatech.edu>,
- */
 case class Diagnostic(patientID: String, date: Date, code: String)
 
 case class LabResult(patientID: String, date: Date, testName: String, value: Double)
