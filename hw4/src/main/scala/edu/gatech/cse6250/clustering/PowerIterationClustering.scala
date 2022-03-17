7b59b6173b4c532711db96c843a21320453d95b6
kristendrex <40031435+kristendrex@users.noreply.github.com>
2022-03-17 12:44:00 -0400

    Update PowerIterationClustering.scala

--- hw4/src/main/scala/edu/gatech/cse6250/clustering/PowerIterationClustering.scala
+++ hw4/src/main/scala/edu/gatech/cse6250/clustering/PowerIterationClustering.scala
@@ -1,6 +1,3 @@
-/**
- * @author Sungtae An <stan84@gatech.edu>.
- */
 
 package edu.gatech.cse6250.clustering
 
@@ -11,9 +8,6 @@ import org.apache.spark.mllib.clustering.{ PowerIterationClustering => PIC }
  * Power Iteration Clustering (PIC), a scalable graph clustering algorithm developed by
  * [[http://www.icml2010.org/papers/387.pdf Lin and Cohen]]. From the abstract: PIC finds a very
  * low-dimensional embedding of a dataset using truncated power iteration on a normalized pair-wise
- * similarity matrix of the data.
- *
- * @see [[http://en.wikipedia.org/wiki/Spectral_clustering Spectral clustering (Wikipedia)]]
  */
 
 object PowerIterationClustering {
